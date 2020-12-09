1c0eeac202f68661bb8c6f135e8e48c4dcee211c
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-09 14:25:46 +0200

    new way of solution

--- Program.cs
+++ Program.cs
@@ -2,25 +2,23 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace _08.TakingTheThreeBigNumbers
+namespace _09.Taking3LargestNumbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)   // split --we get everithign as a string, we move out the whiat espace , we convert it to int , ToArray
         {
-            // 1. Read from the console the n numebers'
-            int n = int.Parse(Console.ReadLine());
-            int[] numbers = new int[n];  // cre3ate an array of inserted numbers
+            int[] numbers = Console.ReadLine().Split(' ')
+                                    .Select(int.Parse)
+                                    .ToArray();
 
-            for (int i = 0; i < n; i++)   // create a for cycle with the exact count of entered numbers // so we enter 5 numbers , 
-                // the array must contain  5 elements
-            {
-                int currNum = int.Parse(Console.ReadLine());  // create a current variable for each entered element and call it by console
-                numbers[i] = currNum;  // every received by the console number must be inerted into the created array to be easy later to sort it
-            }
-            Console.WriteLine("the three largest numbers are : " + string.Join(" ", numbers.OrderByDescending(num => num).Take(3)));
-            // We print the 3 big numbers, but before that we order the array by Descending order so the first 3 numbers will be the largest 3, 
-            // and after the array in sorted descending we take the  first 3 numbers and prin them as a string inline collection
+            // Console.WriteLine(string.Join(" ", numbers.OrderByDescending(num => num).Take(3)));
+
+            int[] largestThreeNumbers = numbers   //We take the existinfg  collection we fetched by the console and create with it new Array
+                                               .OrderByDescending(x => x)  //order the collection by Descending so firsdt 3 nums are the largest
+                                               .Take(3)  //We take the  first 3 largest numbers from old array and we create new array with those 3  numbers 
+                                               .ToArray();
+            Console.WriteLine(string.Join("-> ", largestThreeNumbers));  //We priont as a string the three elements ;
         }
     }
 }
